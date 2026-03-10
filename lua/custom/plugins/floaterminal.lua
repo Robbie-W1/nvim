2b0cca8a27633a301bf2a2f4665e13255fe26cc9
Robbie Woolterton <RobbieWoolterton@gmail.com>
2026-03-10 15:48:49 +0000

    Update terminal config

--- lua/custom/plugins/floaterminal.lua
+++ lua/custom/plugins/floaterminal.lua
@@ -3,6 +3,8 @@ return {
     'numToStr/FTerm.nvim',
     config = function()
       local fterm = require 'FTerm'
+      local last_cmd = nil
+
       vim.keymap.set('n', '<leader>tt', function()
         if vim.bo.modified then
           vim.cmd 'write'
@@ -13,17 +15,37 @@ return {
       vim.keymap.set('t', '<leader>tt', function()
         fterm:toggle()
       end, { desc = '[T]oggle [T]erminal' })
+
+      vim.api.nvim_create_user_command('RunLastCommand', function()
+        if vim.bo.modified then
+          vim.cmd 'write'
+        end
+        if last_cmd == nil then
+          vim.ui.input({ prompt = 'Command to run: ' }, function(input)
+            if input and input ~= '' then
+              last_cmd = vim.split(input, ' ')
+              fterm.scratch { cmd = last_cmd }
+            end
+          end)
+        else
+          fterm.scratch { cmd = last_cmd }
+        end
+      end, { bang = true })
+
+      vim.api.nvim_create_user_command('RedefineCommand', function()
+        if vim.bo.modified then
+          vim.cmd 'write'
+        end
+        vim.ui.input({ prompt = 'Command to run: ' }, function(input)
+          if input and input ~= '' then
+            last_cmd = vim.split(input, ' ')
+            fterm.scratch { cmd = last_cmd }
+          end
+        end)
+      end, { bang = true })
+
+      vim.keymap.set('n', '<leader>r', '<cmd>RunLastCommand<cr>', { desc = '[R]un Last Command' })
+      vim.keymap.set('n', '<leader>rr', '<cmd>RedefineCommand<cr>', { desc = '[R]edefine Command' })
     end,
-
-    vim.api.nvim_create_user_command('CargoRun', function()
-      require('FTerm').scratch { cmd = { 'cargo', 'run' } }
-    end, { bang = true }),
-
-    vim.api.nvim_create_user_command('CargoBuild', function()
-      require('FTerm').scratch { cmd = { 'cargo', 'build' } }
-    end, { bang = true }),
-
-    vim.keymap.set('n', '<leader>cr', '<cmd>CargoRun<cr>', { desc = '[C]argo [R]un' }),
-    vim.keymap.set('n', '<leader>cb', '<cmd>CargoBuild<cr>', { desc = '[C]argo [B]uild' }),
   },
 }
